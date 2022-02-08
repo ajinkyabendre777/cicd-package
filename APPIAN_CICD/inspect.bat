faccc235291e34d08e1d8d6958a8c2c50c8e8dcb
ajinkyabendre777 <ajinkyabendre777@gmail.com>
2022-02-08 23:08:31 +0530

    updated temp folder path

--- APPIAN_CICD/inspect.bat
+++ APPIAN_CICD/inspect.bat
@@ -7,7 +7,7 @@ set apiKey=%3%
 set url="%envBaseUrl%/deployment-management/v1/inspections/"
 set header="Appian-API-Key: %apiKey%"
 
-CD %projectHome%/temp
+CD %projectHome%/cicd/APPIAN_CICD/temp
 
 curl --location --request POST %url% --header %header%  --form "json={\"packageFileName\":\"APPIAN_PACKAGE.zip\",\"customizationFileName\":\"APPIAN_PROPERTIES_FILE.properties\"}" --form "zipFile=@\"APPIAN_PACKAGE.zip\"" --form "ICF=@\"APPIAN_PROPERTIES_FILE.properties\""
 
