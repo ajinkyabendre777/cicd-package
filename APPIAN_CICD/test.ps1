8e6a1530c088738fe6df0eed3259e09c3f5ee9f8
ajinkyabendre777 <32358139+ajinkyabendre777@users.noreply.github.com>
2022-02-09 23:53:49 +0530

    Update test.ps1

--- APPIAN_CICD/test.ps1
+++ APPIAN_CICD/test.ps1
@@ -2,10 +2,8 @@ $appCode = $args[0]
 $jiraNumber = $args[1]
 $targetEnv = $args[2]
 
-$projectHome ="C:\GIT_REPO\coforge-cicd-package\APPIAN_CICD\"+$appCode
+$projectHome ="C:\ProgramData\Jenkins\.jenkins\workspace\EOS\test"
 
-$testPath = $projectHome+"\test"
-
-Set-Location -Path $testPath
+Set-Location -Path $projectHome
 mvn clean install
 
