687ab023172e59267589b4d117d4f31d0ef1d2c6
ajinkyabendre777 <32358139+ajinkyabendre777@users.noreply.github.com>
2022-02-14 23:22:40 +0530

    Update inspect.bat

--- APPIAN_CICD/inspect.bat
+++ APPIAN_CICD/inspect.bat
@@ -9,7 +9,6 @@ set header="Appian-API-Key: %apiKey%"
 
 CD %projectHome%/cicd/APPIAN_CICD/temp
 
-curl -V
 curl --location --request POST %url% --header %header%  --form "json={\"packageFileName\":\"APPIAN_PACKAGE.zip\",\"customizationFileName\":\"APPIAN_PROPERTIES_FILE.properties\"}" --form "zipFile=@\"APPIAN_PACKAGE.zip\"" --form "ICF=@\"APPIAN_PROPERTIES_FILE.properties\""
 
 
